c81eb298e1d74c4c78fb8528514ec666b971f49f
Serenitybleps <fawkesphoenix21@gmail.com>
2023-07-13 13:53:11 -0700

    iteration 1 = barebones app

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -8,19 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var fact1 = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+        ZStack {
+            
+            Color(.systemPink)
+                .ignoresSafeArea()
+            
+            
+            VStack (spacing: 20.0) {
+               
+                Text("Kylie N")
+                
+        
+                
+                Image("beach")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius (15)
+            
+                
+                Image("tech")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius (15)
+                
+                  
+                
+                HStack {
+                    Button("Facts About Me") {
+                        fact1 = "I am a rising senior and I like cats"
+                        
+
+                        
+    
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
+            
+            
+            
+                    
+                    
+                }
+                Text(fact1)
+                
+                
+            }
+            
+
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
